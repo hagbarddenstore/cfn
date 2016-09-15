21406ca24f3f5081ac1286418befef2661b680d9
Kim Johansson <kim@kumiko.se>
2016-09-15 09:04:27 +0200

    Change boto version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__FILE__))
+here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 	long_description = f.read()
@@ -24,7 +24,7 @@ setup(
 	],
 	keywords="aws cfn cloudformation",
 	packages=find_packages(exclude=["contrib", "docs", "tests"]),
-	install_requires=["boto4", "PyYAML", "jinja2"],
+	install_requires=["boto3", "PyYAML", "jinja2"],
 	extras_require={},
 	package_data={},
 	data_files=[],
