aae58e8b65a5b9dd32d9bf451cd89e2e1672dc5e
Kim Johansson <kim@kumiko.se>
2016-09-26 20:09:53 +0200

    Revert back to using rst file

--- setup.py
+++ setup.py
@@ -4,20 +4,12 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-try:
-	import pypandoc
-
-	long_description = pypandoc.convert("README.md", "rst")
-	long_description = long_description.replace("\r", "")
-except (OSError, ImportError):
-	print "Pandoc not found. long_description conversion failure."
-
-	with open("README.md", encoding="utf-8") as f:
-		long_description = f.read()
+with open("README.rst", encoding="utf-8") as f:
+  long_description = f.read()
 
 setup(
 	name="cfn",
-	version="0.0.7",
+	version="0.0.9",
 	description="Small script to manipulate AWS CloudFormation stacks",
 	long_description=long_description,
 	url="https://github.com/hagbarddenstore/cfn",
@@ -34,7 +26,9 @@ setup(
 	packages=find_packages(exclude=["contrib", "docs", "tests"]),
 	install_requires=["boto3", "PyYAML", "jinja2"],
 	extras_require={},
-	package_data={},
+	package_data={
+		"": [ "*.md", "*.txt", "*.rst" ]
+	},
 	data_files=[],
 	entry_points={
 		"console_scripts": [
